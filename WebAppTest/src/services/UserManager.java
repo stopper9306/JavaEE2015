b2fe9837aff40eb649b85455e53666dff6dfad5b
npanayotova173@gmail.com <npanayotova>
2015-06-28 22:46:29 +0300

    ...

--- WebAppTest/src/services/UserManager.java
+++ WebAppTest/src/services/UserManager.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import slbedu.library.model.User;
+import models.User;
 
 public class UserManager {
 	
