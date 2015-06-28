6c0e9c4620ef24e2dd53d3f6e41e7cc7e0b9bb3b
npanayotova173@gmail.com <npanayotova>
2015-06-28 20:45:25 +0300

    class UserConext , class UserManager

---
+++ WebAppTest/src/services/UserManager.java
@@ -0,0 +1,30 @@
+package services;
+
+import java.io.IOException;
+import java.sql.Array;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import slbedu.library.model.User;
+
+public class UserManager {
+	
+    private UserContext context;
+	
+    public String getUser() {
+        if (context.getCurrentUser() == null) {
+            return null;
+        }
+        return context.getCurrentUser().getUserName();
+    }
+    
+	public void logoutUser() {
+		context.setCurrentUser(null);
+	}
+	
+	
+}
