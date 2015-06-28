389bbf2f7c784ea91a91469566358d97daf72bc5
georgi <geshavn@gmail.com>
2015-06-28 20:49:15 +0300

    added second interval

--- WebAppTest/src/services/CommentsManager.java
+++ WebAppTest/src/services/CommentsManager.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class CommentsManager extends HttpServlet{ 
+public class CommentsManager extends HttpServlet{  
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		/*resp.setStatus(200);
