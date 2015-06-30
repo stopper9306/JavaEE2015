37dd8f0753f912c6f50b2a26ce0f007a208433b0
Denis Bialev <stopper9306@gmail.com>
2015-06-30 22:49:54 +0300

    Updates on User table Column Names and added constants.

--- WebAppTest/src/models/User.java
+++ WebAppTest/src/models/User.java
@@ -7,77 +7,87 @@ import com.j256.ormlite.table.DatabaseTable;
 
 @DatabaseTable(tableName = "users")
 public class User {
-	
-    @DatabaseField(id = true)
+
+    public static final String USER_NAME = "UserName";
+
+    public static final String FULL_NAME = "FullName";
+
+    public static final String PASSWORD = "Password";
+
+    public static final String EMAIL = "Email";
+
+    public static final String USER_TYPE = "UserType";
+
+    @DatabaseField(columnName = USER_NAME, unique = true, canBeNull = false)
     private String userName;
 
-    @DatabaseField
+    @DatabaseField(columnName = FULL_NAME, canBeNull = false)
     private String fullName;
 
-    @DatabaseField
+    @DatabaseField(columnName = PASSWORD, canBeNull = false)
     private String password;
 
-    @DatabaseField
+    @DatabaseField(columnName = EMAIL, canBeNull = false)
     private String email;
 
-    @DatabaseField
+    @DatabaseField(columnName = USER_TYPE, canBeNull = false)
     private UserType type;
-    
+
     private List<Task> tasks;
 
-	public User() {
+    public User() {
 	// ORMLite needs a no-arg constructor
     }
 
     public User(String userName, String fullName, String password, String email, UserType type) {
-		this.userName = userName;
-		this.fullName = fullName;
-		this.password = password;
-		this.email = email;
-		this.type = type;
+	this.userName = userName;
+	this.fullName = fullName;
+	this.password = password;
+	this.email = email;
+	this.type = type;
     }
 
     public String getUserName() {
-    	return userName;
+	return userName;
     }
 
     public void setUserName(String userName) {
-    	this.userName = userName;
+	this.userName = userName;
     }
 
     public String getFullName() {
-    	return fullName;
+	return fullName;
     }
 
     public void setFullName(String fullName) {
-    	this.fullName = fullName;
+	this.fullName = fullName;
     }
 
     public String getPassword() {
-    	return password;
+	return password;
     }
 
     public void setPassword(String password) {
-    	this.password = password;
+	this.password = password;
     }
 
     public String getEmail() {
-    	return email;
+	return email;
     }
 
     public void setEmail(String email) {
-    	this.email = email;
+	this.email = email;
     }
 
     public UserType getType() {
-    	return type;
+	return type;
     }
 
     public void setType(UserType type) {
-    	this.type = type;
+	this.type = type;
     }
-    
+
     public List<Task> getTasks() {
-		return tasks;
-	}
+	return tasks;
+    }
 }
