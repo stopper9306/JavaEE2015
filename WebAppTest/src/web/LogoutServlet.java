8c477db5044fd32dafbd8d94176506bd1e04bbb3
npanayotova <npanayotova173@gmail.com>
2015-07-03 05:32:42 +0300

    Added logout functionality and fixed the UI

---
+++ WebAppTest/src/web/LogoutServlet.java
@@ -0,0 +1,34 @@
+package web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class LogoutServlet
+ */
+@WebServlet("/LogoutServlet")
+public class LogoutServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)  
+            throws ServletException, IOException {   
+    	HttpSession session  = request.getSession();
+    	if(session !=null)
+    	try
+    	{      
+    	    session.removeAttribute("logonSessData");
+        	request.getSession().invalidate();
+        	response.sendRedirect(request.getContextPath() + "/index.html");          
+		} catch (Exception e) {
+		e.printStackTrace();
+		}
+}  
+
+}
